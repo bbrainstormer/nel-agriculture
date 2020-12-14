373b2f00472d30b382f42c43928c00b0b0030a75
bbrainstormer123 <60482994+bbrainstormer123@users.noreply.github.com>
2020-12-14 16:37:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -14,40 +14,32 @@ G.AddData({
                     'health': -0.2,
                     'happiness': -0.3
                     },
-                    'decay': {
-                        'spoiled food': 0 //Decays into nothing
-                        }
+                'decay': {
+                    'spoiled food': 0 //Decays into nothing
+                    }
                 }, 
-                partOf: '',
-                category: 'food',
+                partOf: '', //People won't eat it
+                category: 'food', // but it'll show up in the menu
             });
 
         //Gather grass blades from, well, grass
-        G.getDict('grass').res['gather']['grass blades'] = 15;
+        G.getDict('grass').res['gather']['grass blades'] = 8;
 
         //Since grass blades are an awful food source, we need to be able to disable that
         //We don't want it dragging health & happiness down
         new G.Policy({
-            name: 'eat grass',
-            desc: 'enables/disables the eating of [grass blades]',
-            cost: {
-                'influence': 1
-            },
-            startMode: 'off',
-            req: {
-                'rules of food': true
-                },
-            effects: [{
-                type: 'make part of',
-                what: ['grass blades'],
-                parent: 'food'
-                }],
-            effectsOff: [{
-                type: 'make part of',
-                what: ['grass blades'],
-                parent: '',
-                category: 'food',
-                }],
-            });
+		    name:'eat grass',
+		    desc:'Your people will eat [grass blades].  An awful source of subsistence, but better than [spoiled food].',
+		    cost:{'influence':1},
+		    startMode:'off',
+		    req:{'rules of food':true},
+		    effects:[
+			    {type:'make part of',what:['grass blades'],parent:'food'},
+		    ],
+		    effectsOff:[
+			    {type:'make part of',what:['grass blades'],parent:''},
+		    ],
+		    category:'food',
+	        });
         }
     });
